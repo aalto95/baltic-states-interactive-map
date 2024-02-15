bdf8ac14cb5798a589c702ac0d7a463f490c2997
aalto95 <stanportableuse@gmail.com>
2024-02-15 15:59:48 +0900

    Remove webpack

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import store from "./redux/redux-store";
 import "./index.css";
 import App from "./App";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
@@ -15,8 +14,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
