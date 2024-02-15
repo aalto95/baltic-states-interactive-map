70b24f861d755fe88fcebfa2435ffa352722b342
aalto95 <stanportableuse@gmail.com>
2024-02-15 15:50:50 +0900

    Add vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import store from './redux/redux-store'
+import store from "./redux/redux-store";
 import "./index.css";
 import App from "./App";
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-  ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>, document.getElementById("root")
-  )
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
