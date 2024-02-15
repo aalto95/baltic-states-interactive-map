c577c6d97f67f38031f1c062fe27e8450f87b2be
aalto95 <stanportableuse@gmail.com>
2024-02-15 17:01:59 +0900

    Update packages and remove unused ones

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import store from "./redux/redux-store";
 import "./index.css";
 import App from "./App";
-import ReactDOM from "react-dom";
-
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("root not found");
+}
+
+const container = createRoot(root);
+
+container.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
